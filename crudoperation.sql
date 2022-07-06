81a58ff569ed233cf7bc90505d92cd112397d094
uemelife <ucheemelife@gmail.com>
2022-07-05 20:50:06 -0400

    Database
    Please upload to your phpmyadmin

---
+++ crudoperation.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2022 at 02:48 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crudoperation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crud`
+--
+
+CREATE TABLE `crud` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `crud`
+--
+
+INSERT INTO `crud` (`id`, `name`, `email`, `mobile`, `password`) VALUES
+(3, 'user3', 'user3@email.com', '0111111112', '@p22a3'),
+(4, 'user4', 'user4@email.com', '444444444', '424'),
+(5, 'user5', 'user5@email.com', '555555555555', '123@456'),
+(6, 'usersname', 'user@email.com', '0100010001', '1020229'),
+(7, 'usersname', 'user@email.com', '20202020202', '1020229');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `crud`
+--
+ALTER TABLE `crud`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `crud`
+--
+ALTER TABLE `crud`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
